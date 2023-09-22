f6a801865bc06950a9c27981ff4f732639de20d9
analytics-chan <ga4.sychoi@gmail.com>
2023-09-22 17:04:12 +0900

    python crawling chapter5-3

---
+++ chapter5/chapter5-3.py
@@ -0,0 +1,56 @@
+# 광고 상품 제외하기
+import requests
+from bs4 import BeautifulSoup
+import time
+
+keyword = input('검색어를 입력하세요 >>> ')
+
+main_url = f"https://www.coupang.com/np/search?q={keyword}&channel=user"
+
+# 헤더에 User-Agent를 추가하지 않으면 에러 발생(멈춤 현상)
+header = {
+    'Host': 'www.coupang.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0',
+    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3',
+}
+
+response = requests.get(main_url, headers=header)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+# 결과는 리스트 자료형
+links = soup.select('a.search-product-link')
+# print(links)
+
+for link in links:
+    # 광고 상품 제거
+    if len(link.select('span.ad-badge-text')) > 0:
+        print('광고 상품입니다.')
+    else:
+        sub_url = 'https://www.coupang.com/' + link.attrs['href']
+
+        response = requests.get(sub_url, headers=header)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # 브랜드명은 있을 수도 있고 없을 수도 있음
+        # 중고상품일 경우 태그가 달라짐
+        # try - except로 예외처리
+        try:
+            brand_name = soup.select_one('a.prod-brand-name ').text.strip()
+        except:
+            brand_name = ""
+
+        # 상품명
+        product_name = soup.select_one('h2.prod-buy-header__title').text
+
+        # 가격
+        try:
+            price = soup.select_one('span.total-price > strong').text
+        except:
+            price = 0
+
+        print(brand_name, product_name, price)
+
+        # time.sleep(0.3)
