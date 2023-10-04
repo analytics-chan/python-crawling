11c1b746e480207bcd87679d6c4600def1801e1c
analytics-chan <ga4.sychoi@gmail.com>
2023-10-04 16:46:03 +0900

    python crawling chapter8-3

--- chapter8/chapter8-3.py
+++ chapter8/chapter8-3.py
@@ -1,14 +1,21 @@
 # 구글 이미지 크롤링 및 큰 이미지 다운로드
+# click_intercepted Error 해결
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+
+# 시간차 처리
 import time
+
+# 폴더 생성
 import os
-# import requests
-# from urllib import request
-# from urllib.request import Request, urlopen
+
+# 파일 저장
 import urllib.request
+
+# 크롬 드라이버 자동 업데이트
 from webdriver_manager.chrome import ChromeDriverManager
 
 keyword = input('검색어를 입력해주세요 >>> ')
@@ -18,8 +25,12 @@ if not os.path.exists(f'chapter8/{keyword}'):
 
 url = f"https://www.google.com/search?sca_esv=568723682&hl=ko&sxsrf=AM9HkKmVilDIuKnlc4pOE-SLOJjz5pPRbw:1695788470987&q={keyword}&tbm=isch&source=lnms&sa=X&ved=2ahUKEwiso7Wr-MmBAxXzk1YBHQ-cC7sQ0pQJegQICxAB&biw=2560&bih=1283&dpr=1"
 
+# 브라우저 꺼짐 방지
 chrome_options = Options()
 chrome_options.add_experimental_option('detach', True)
+
+# 불필요한 에러 메시지 삭제
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
@@ -46,7 +57,14 @@ imgs = driver.find_elements(By.CSS_SELECTOR, '.rg_i.Q4LuWd')
 
 for i, img in enumerate(imgs, 1):
     # 이미지 클릭 후 큰 사이즈 추출
-    img.click()
+    # 클릭하다보면 element click intercepted Error 발생
+
+    # JavaScript로 클릭을 직접 하도록 만들어서 해결
+    driver.execute_script('arguments[0].click();', img)
+
+    # 셀레니움으로 직접 클릭
+    # img.click()
+
     time.sleep(1)
 
     # 큰 이미지 주소 추출
@@ -58,6 +76,7 @@ for i, img in enumerate(imgs, 1):
     #     imgUrl = driver.find_element(By.CSS_SELECTOR, '.r48jcc.pT0Scc').get_attribute('src')
     #     print(i, imgUrl)
     
+    # 이미지 url 주소 에러 방지
     try:
         imgUrl = driver.find_element(By.CSS_SELECTOR, '.r48jcc.pT0Scc.iPVvYb').get_attribute('src')
     except:
@@ -68,6 +87,13 @@ for i, img in enumerate(imgs, 1):
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
-    urllib.request.urlretrieve(imgUrl, f'chapter8/{keyword}/{i}.jpg')
+
+    # 이미지 다운로드 시 오류나는 이미지 제외하고 저장
+    try:
+        urllib.request.urlretrieve(imgUrl, f'chapter8/{keyword}/{i}.jpg')
+    except:
+        pass
+
+    # 50개까지만 저장
     if i == 50:
         break
