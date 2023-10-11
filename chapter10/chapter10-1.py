d8f7e3f84ca3ffd82d39554e517a0365942c7bc9
analytics-chan <ga4.sychoi@gmail.com>
2023-10-11 16:15:35 +0900

    python crawling chapter10-2

--- chapter10/chapter10-1.py
+++ chapter10/chapter10-1.py
@@ -15,8 +15,18 @@ trs = soup.select("table.type_2 > tbody > tr[onmouseover='mouseOver(this)']")
 for tr in trs:
     # nth-child 사용 방법
     name = tr.select_one('td:nth-child(2)').text
-    per = tr.select_one('td:nth-child(7)').text
-    roe = tr.select_one('td:nth-child(8)').text
-    pbr = tr.select_one('td:nth-child(9)').text
-    reserve_ratio = tr.select_one('td:nth-child(10)').text
-    print(name, per, roe, pbr, reserve_ratio)
+    per = tr.select_one('td:nth-child(8)').text
+    roe = tr.select_one('td:nth-child(9)').text
+    pbr = tr.select_one('td:nth-child(10)').text
+    reserve_ratio = tr.select_one('td:nth-child(11)').text
+
+    # 데이터 전처리 => N/A 값이 아닐 경우에만
+    if per != 'N/A' and roe != 'N/A' and pbr != 'N/A' and reserve_ratio != 'N/A':
+        per = float(per.replace(',', ''))
+        roe = float(roe.replace(',', ''))
+        pbr = float(pbr.replace(',', ''))
+        reserve_ratio = float(reserve_ratio.replace(',', ''))
+        print(name, per, roe, pbr, reserve_ratio)
+
+     
+    # print(name, per, roe, pbr, reserve_ratio)
