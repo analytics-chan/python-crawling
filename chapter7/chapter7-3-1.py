52f26ce4bb0ac2fdeb973308a7b58957b9a60e31
analytics-chan <ga4.sychoi@gmail.com>
2023-11-23 14:45:35 +0900

    fixed naver place test code

--- chapter7/chapter7-3-1.py
+++ chapter7/chapter7-3-1.py
@@ -7,24 +7,47 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
 import time
-import openpyxl
+import datetime
+import os
+from openpyxl import Workbook, load_workbook
+
+today = datetime.datetime.today()
+year = str(today.year)
+month = str(today.month)
+day = str(today.day)
 
 # keyword = input('검색어를 입력하세요 >>> ')
 keyword = "더와이즈치과병원"
 
+path = 'chapter7'
+file_list = os.listdir(path)
+file_name = f'{year}{month}{day} {keyword}_리뷰.xlsx'
+
+if file_name in file_list:
+    wb = load_workbook(os.path.join(path, file_name))
+else:
+    wb = Workbook()
+
+    ws = wb.create_sheet('네이버플레이스_리뷰', 0)
+
+ws = wb.active
+
+ws['A1'] = 'Date'
+ws['B1'] = '닉네임'
+ws['C1'] = '작성리뷰수'
+ws['D1'] = '작성일자'
+ws['E1'] = '방문수'
+ws['F1'] = '인증수단'
+ws['G1'] = '내용'
+
+ws.column_dimensions['A'].width = 20
+ws.column_dimensions['B'].width = 20
+ws.column_dimensions['C'].width = 10
+ws.column_dimensions['D'].width = 11
+ws.column_dimensions['F'].width = 10
+
 url = f"https://map.naver.com/p/search/{keyword}"
 # url = f"https://map.naver.com/p/search/{keyword}/place/33084820?c=15.00,0,0,0,dh&isCorrectAnswer=true"
-
-wb = openpyxl.Workbook()
-ws = wb.create_sheet(keyword, 0)
-
-ws.column_dimensions['A'].width = 15
-ws.column_dimensions['B'].width = 12
-ws.column_dimensions['C'].width = 70
-ws.column_dimensions['D'].width = 25
-ws.column_dimensions['E'].width = 15
-
-ws.append(['작성자', '작성 리뷰수', '작성리뷰', '날짜', '방문수', '인증 수단'])
 
 chrome_options = Options()
 chrome_options.add_experimental_option('detach', True)
@@ -70,19 +93,22 @@ try:
         # driver.find_element(By.XPATH, '//*[@id="app-root"]/div/div/div/div[6]/div[3]/div[3]/div[2]/a').click()
         driver.find_element(By.CSS_SELECTOR, '#app-root > div > div > div > div:nth-child(6) > div:nth-child(3) > div.place_section.k5tcc > div.NSTUp > div > a > span').click()
 
+        time.sleep(1)
+
         lis = driver.find_elements(By.CSS_SELECTOR, 'li.YeINN')
         after_len = len(lis)
         print(after_len)
         
         time.sleep(1)
 
-        if after_len == 10:
-            break
-
-        # if before_len == after_len:
+        # if after_len == 30:
         #     break
 
-        # before_len = after_len
+        if after_len == before_len:
+            break
+
+        before_len = after_len
+        print(before_len)
 
         # if i == 10:
         #     break
@@ -102,119 +128,74 @@ try:
             # find_review = review.find('div.ZZ4OK > a > span.rvCSr > svg')
             # print(find_review)
             more_content = review.find_element(By.CSS_SELECTOR, 'div.ZZ4OK > a > span.rvCSr > svg')
-            print(more_content)
+            # print(more_content)
             more_content.click()
+            print('클릭완료')
+
             time.sleep(1)
 
-            user_review = review.find_element(By.CSS_SELECTOR, 'div.ZZ4OK > a > span.zPfVt')
+            # user_review = review.find_element(By.CSS_SELECTOR, 'div.ZZ4OK > a > span.zPfVt')
         except:
-            user_review = review.find_element(By.CSS_SELECTOR, 'div.ZZ4OK > a > span.zPfVt')
-
-            time.sleep(1)
+            # user_review = review.find_element(By.CSS_SELECTOR, 'div.ZZ4OK > a > span.zPfVt')
+            pass
+            # time.sleep(1)
 except:
     time.sleep(1)
     print('리뷰가 존재하지 않음')
 
+driver.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.END)
 
+res = driver.page_source
+soup = BeautifulSoup(res, 'html.parser')
 
+reviews = soup.select('li.YeINN')
+print(len(reviews))
 
-# # driver.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.END)
+for review in reviews:
+    nick = review.select_one('div.VYGLG').text
 
-# reviews_s = driver.find_elements(By.CSS_SELECTOR, 'li.YeINN')
-# # print(len(reviews))
-
-# driver.find_element(By. CSS_SELECTOR, '#app-root > div > div > div > div:nth-child(6) > div:nth-child(3) > div.place_section.k5tcc > div.place_section_content > ul > li:nth-child(4) > div.ZZ4OK > a > span.rvCSr').click()
-# # print(len(etc))
-# # print(etc)
-
-# #app-root > div > div > div > div:nth-child(6) > div:nth-child(3) > div.place_section.k5tcc > div.place_section_content > ul > li:nth-child(4) > div.ZZ4OK > a > span.rvCSr
-
-# time.sleep(1)
-
-# # try:
-# #     for e in etc:
-# #         e.click()
-# # except:
-# #     pass
-
-# # for s in reviews_s:
-# #     span_len = s.find_elements(By.CSS_SELECTOR, 'a.xHaT3 > span')
-# #     print(len(span_len))
-
-# #     try:
-# #         s.find_element(By. CSS_SELECTOR, 'a.xHaT3 > span:nth-child(2) > svg > path').click()
-# #     except:
-# #         pass
-
-#     # if len(span_len) == 2:
-#     #     s.find_element(By. CSS_SELECTOR, 'a.xHaT3 > span:nth-child(2) > svg > path').click()
-#     # else:
-#     #     pass
-
+    try:
+        review_cnt = review.select_one('span.Eu5rp').text[2:].strip()
+    except:
+        review_cnt = 0
     
-#     # try:
-#     #     # etc = driver.find_element(By. CSS_SELECTOR, 'span.rvCSr')
-#     #     # etc.click()
-#     #     driver.find_element(By. CSS_SELECTOR, 'span.rvCSr').click()
+    try:
+        user_review = review.select_one('div.ZZ4OK > a > span.zPfVt').text.replace('\n', ' ')
+    except:
+        user_review = ""
 
-#     #     etc = driver.find_element(By. CSS_SELECTOR, 'span.rvCSr')
+    try:
+        ymd = review.select_one('div._7kR3e > span:nth-child(1) > span:nth-child(3)').text[:-4]
+
+        yy = ymd.split('년')[0].strip()
+        mm = ymd.split('년')[1].split('월')[0].strip()
+        dd = ymd.split('년')[1].split('월')[1][:-1].strip()
         
-
-#     #     time.sleep(1)
-
-#     #     print('클릭완료')
-#     # except:
-#     #     pass
-#     #     print('pass')
-
-# # etc = driver.find_elements(By.CSS_SELECTOR, 'span.rvCSr > svg')
-# # print(len(etc))
-
-# # for e in etc:
-# #     e.click()
-
-# # res = driver.page_source
-# # soup = BeautifulSoup(res, 'html.parser')
-
-# # reviews = soup.select('li.YeINN')
-# # print(len(reviews))
-
-# # for r in reviews:
-
-# #     try:
-# #         etc = driver.find_element(By. CSS_SELECTOR, 'span.rvCSr')
-# #         etc.click()
+        if len(mm) == 1:
+            mm = '0' + mm
         
-# #         time.sleep(1)
+        if len(dd) == 1:
+            dd = '0' + dd
 
-# #         print('클릭완료')
+        write_date = f'{yy}-{mm}-{dd}'
+    except:
+        write_date = ""
 
-# #     except:
-# #         pass
-# #         print('패스')
+    try:
+        visit = review.select_one('div._7kR3e > span:nth-child(2)').text[:-5]
+    except:
+        visit = ""
 
-# #     time.sleep(1)
+    try:
+        cert = review.select_one('div._7kR3e > span:nth-child(3)').text[5:]
+    except:
+        cert = ""
 
-# #     nick = r.select_one('div.VYGLG').text
+    print(nick, review_cnt, write_date, visit, cert, user_review)
+    ws.append([today.now(), nick, int(review_cnt), write_date, int(visit), cert, user_review])
 
-# #     print(nick)
+end_txt = f"{today.now()} 크롤링 완료"
+ws.append([end_txt])
 
+wb.save(os.path.join(path, file_name))
 
-
-
-
-
-
-
-
-#     # etc = r.find_element(By.CSS_SELECTOR, 'span.rvCSr')
-
-#     # print(bool(etc))
-    
-#     # etc.click()
-
-#     # etc = driver.find_element(By.CSS_SELECTOR, 'span.rvCSr > svg').click()
-#     # print(len(etc))
-
-#     # for e in etc:
-#     #     e.click()
